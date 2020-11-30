c9b686c9f89493048a848cda926d0cf046bc646c
yonathan4 <70810810+yonathan4@users.noreply.github.com>
2020-11-30 14:49:45 +0200

    Add files via upload

---
+++ fylo/app.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
